fde9f4acd791f9c7fec1853cd3aa126c41c49315
Thendo <thendomakherana@gmail.com>
2022-02-06 22:54:24 +0200

    changes

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,26 +1,29 @@
-import { GoogleOutlined, FacebookOutlined } from '@ant-design/icons';
-import React from 'react';
+import { GoogleOutlined, FacebookOutlined } from "@ant-design/icons";
+
+import React from "react";
+
+import firebase from "firebase";
+
+import { auth } from "firebase/app";
 
 const Login = () => {
-    return(
-        <div id="login-page">
-            <div id="login-container">
-                <h2>Welcome to WeMeet!</h2>
-                <div className="login-button goggle">
-                    <GoogleOutlined /> Sign in with google
-                    <br/> <br/>
-
-
-                    <div className="login-button facebook">
-                    <FacebookOutlined /> Sign in with facebook
-            
-                    </div>
-                    </div>
-
-                </div>
+  return (
+    <div id="login-page">
+      <div id="login-card">
+        <div id="login-container">
+          <h2>Welcome to WeMeet!</h2>
+          <div className="login-button google">
+            <GoogleOutlined /> Sign in with google
+          </div>
+          <br></br>
+          <br></br>
+          <div className="login-button facebook">
+            <FacebookOutlined /> Sign in with facebook
+          </div>
         </div>
-
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 export default Login;
---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+
+export const auth = firebase.initializeApp({ 
+    apiKey: "AIzaSyBpe3jTh8_7IkiRuJDpm8Tzj-w-llDefxo",
+    authDomain: "wemeet-c4ef8.firebaseapp.com",
+    projectId: "wemeet-c4ef8",
+    storageBucket: "wemeet-c4ef8.appspot.com",
+    messagingSenderId: "1002575193741",
+    appId: "1:1002575193741:web:e90d211e9ef0fde7f124c4"
+  })
