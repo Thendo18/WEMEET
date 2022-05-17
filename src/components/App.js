96f263efbb004da06eba284a7359d115f7450526
Thendo18 <thendomakherana@gmail.com>
2022-05-17 12:36:05 +0200

    fixing errors

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,7 @@
 import React from "react"
-
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
-
 import { AuthProvider } from "../contexts/AuthContext"
-
-import Chats from "./Chats"
+// import Chats from "./Chats"
 import Login from "./Login"
 
 function App() {
@@ -13,7 +10,7 @@ function App() {
       <Router>
         <AuthProvider>
           <Switch>
-              <Route path="/chats" component={Chats} />
+              {/* <Route path="/chats" component={Chats} /> */}
             <Route path="/" component={Login} /> 
           </Switch>
         </AuthProvider>
--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,5 @@
 import firebase from "firebase/app";
-
+import "firebase/auth";
 
 export const auth = firebase.initializeApp({ 
     apiKey: "AIzaSyBpe3jTh8_7IkiRuJDpm8Tzj-w-llDefxo",
@@ -8,4 +8,4 @@ export const auth = firebase.initializeApp({
     storageBucket: "wemeet-c4ef8.appspot.com",
     messagingSenderId: "1002575193741",
     appId: "1:1002575193741:web:e90d211e9ef0fde7f124c4"
-  })
+  }).auth();
