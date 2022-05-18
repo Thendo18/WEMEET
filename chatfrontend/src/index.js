cc461609eb23d28ae36173bd7af05ae54e54c421
Thendo18 <thendomakherana@gmail.com>
2022-05-18 08:08:13 +0200

    changes

--- chatfrontend/src/index.js
+++ chatfrontend/src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 
+// import App from "./App";
+import rootReducers from "./store/reducer/index";
+import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import React from "react";
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
